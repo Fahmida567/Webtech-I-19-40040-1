1cffe4f40828be9f13158bd50d9b1516d9e8ae0c
Turin <you@example.com>
2021-02-27 14:04:44 +0600

    lab task 3 and 4 updated

--- Lab Task 3/form.php
+++ Lab Task 3/form.php
@@ -5,13 +5,11 @@
 <form action="profile picture  validation.php" method="post" enctype="multipart/form-data">
 	<fieldset>
 		 <legend>  Profile Picture:</legend> 
-		<img src="img.png" alt="" width="50" height="60">
-<br><br>
+		<tr><th align ="left"><h3></h3>
+	<img src="images/img.png" align="left" height="200" width="200"/>
+<br><br><br>
   
-  <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+  <br><br><br><br><br><br><br><br><br><br><input type="file" name="fileToUpload" id="fileToUpload"><br><br>
   <input type="submit" value="Submit" name="submit">
 </form>
 </fieldset>
-
-</body>
-</html>
