031f3a127896cbad99270eea08bc9626128d3753
Turin <you@example.com>
2021-04-05 15:13:57 +0600

    task 6 done

--- Lab Task 6/View/dashboardStudentView.php
+++ Lab Task 6/View/dashboardStudentView.php
@@ -1,43 +1,24 @@
-   <fieldset>
-<legend>DASHBOARD</legend>
-
-<?php
+  <fieldset>
+	<legend>DASHBOARD</legend>
+ <?php
 session_start();
 
+if(isset($_SESSION['username']))
+{
 
-
-if (isset($_POST['uname'])) {
-  if ($_POST['uname']==$username && $_POST['pass']==$password) {
-    $_SESSION['uname'] = $username;
-    header("location:Dashboard.php");
-  }
-  else{
-    $msg="username or password invalid";
-    echo "<script>alert('uname or pass incorrect!')</script>";
-  }
-
-} 
-
-?> 
-     <ul><li><a href="dashboardStudentView.php">Dashboard</a></li>
-		<li><a href="viewProfileStudentView.php">View Profile</a></li>
-		<li><a href="editProfileStudentView.php">Edit Profile</a></li>
-        <li><a href="changePasswordStudentView.php">Change Password</a></li>
-		<li><a href="../Controller/logoutStudentController.php">Logout</a></li>
-		</li></ul>
-		
-
-
-<?php echo "<h2>Welcome Fahmida</h2>"; ?>
-<?php 
- include 'footerView.php';?>
- 
- 
-  
+ echo "<br><a href='dashboardStudentView.php'>Dashboard</a>";
+ echo "<br><a href='viewProfileStudentView.php'>View Profile</a>";
+ echo "<br><a href='editProfileStudentView.php'>Edit Profile</a>";
+ // echo "<br><a href='upload.php'>Change Profile Picture</a>";
+  echo "<br><a href='changePasswordStudentView.php'>Change Password</a>";
+  echo "<br><a href ='../Controller/logoutStudentController.php'>Logout </a>";
+  echo "<h1 align='middle'> Welcome ".$_SESSION['username']."</h2>";
 
 
 
-
-</body>
-</html>
-
+}
+else {
+  echo "You can not access the page.";
+}
+ ?>
+ </fieldset>
